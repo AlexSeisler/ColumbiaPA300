1a558321e36682bac2370991bb2f923a54a8ae0b
Alex Seisler <aseisler7045@stevenscollege.edu>
2025-05-15 19:01:08 -0400

    Initial setup: Vite + React + Home route scaffolded

---
+++ columbiapa300.com/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <main>
+      <h1>Columbia Borough 300th Anniversary</h1>
+      <p>Welcome to the official campaign site: columbiapa300.com</p>
+    </main>
+  )
+}
+
+export default Home
